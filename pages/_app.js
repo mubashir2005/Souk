550609f36de989a2839f14207eff9d5849730e44
dingus45191 <mubashirhasan716@gmail.com>
2021-12-24 23:19:10 +0530

    Started building the frontend

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,26 @@
+import Link from 'next/link'
 import '../styles/globals.css'
+import 'tailwindcss/tailwind.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+    <div>
+    <nav className='border-b p-6'>
+      <p className='text-4xl font-bold '>Souk NFT</p>
+      <div className='flex mt-4'></div>
+      <Link href={'/home'}>
+      <a className='mr-6 text-pink-500'>Home</a>
+      </Link>
+            <Link href={'/sell'}>
+      <a className='mr-6 text-pink-500'>Sell</a>
+      </Link>
+            <Link href={'/assets'}>
+      <a className='mr-6 text-pink-500'>My assets</a>
+      </Link>
+    </nav>
+    <Component {...pageProps} />
+    </div>
+  ) 
 }
 
 export default MyApp
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
-  content: [],
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
   theme: {
+     extend: {},
+  },
+  variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: []
 }
